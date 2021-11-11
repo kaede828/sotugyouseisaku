7e70ad802e234b78cb11facd39db98d3b8b4544a
kaede828 <kaede1999828@gmail.com>
2021-11-11 20:05:00 +0900

    コミット２

---
+++ sotugyouseisaku/Assets/koike/Scripts/Flash.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash : MonoBehaviour
+{
+    private Image img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        img.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            img.color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            img.color = Color.Lerp(img.color, Color.clear, Time.deltaTime);
+        }
+    }
+}
