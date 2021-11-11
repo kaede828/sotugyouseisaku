f024acb40dc4472370cd4874ced2ee5fe6a8d3a4
hryuh1121 <h.ryuh1121@gmail.com>
2021-11-11 19:05:10 +0900

    opのテキストとスクリプト変えた

--- sotugyouseisaku/Assets/Honda/TextDisplay.cs
+++ sotugyouseisaku/Assets/Honda/TextDisplay.cs
@@ -14,12 +14,6 @@ public class TextDisplay : MonoBehaviour
     public float second = 1;//テキスト表示を何秒待つか
     bool isTimer;//タイマーのスタート
     public Image texthaikei;//テキストの後ろの透過イメージ
-    bool push;//テキストの最後にボタンが押されたか
-    bool textend;//テキストが最後まで再生されたか
-    public Image setumeisyo;//説明テキスト
-    bool Ybutton;
-    bool PreYbutton;//1フレーム前の入力
-    public Text ytext;//右上のテキスト
 
     void Start()
     {
@@ -28,37 +22,8 @@ public class TextDisplay : MonoBehaviour
         displayTextSpeed = 0;
         textStop = false;
         isTimer = false;
-        setumeisyo.enabled = false;
-        Ybutton = false;
-        ytext.enabled = false;
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown("joystick button 3"))
-        {
-            Ybutton = true;
-        }
-
-        if (textend == true && Ybutton)//bボタンが押されたら
-        {
-            push = true; //押された判定にする
-        }
-        if (textend == true)
-        {
-            if (setumeisyo.enabled == true && Ybutton&&!PreYbutton)
-            {//説明が表示されていたら非表示にする
-                setumeisyo.enabled = false;
-            }
-            else if (setumeisyo.enabled == false && Ybutton&&!PreYbutton)
-            {//説明が非表示なら表示する
-                setumeisyo.enabled = true;
-            }
-        }
-
-        PreYbutton = Ybutton;//1フレーム前の入力
-        Ybutton = false;
-    }
     void FixedUpdate()
     {
         if (isTimer)
@@ -69,7 +34,7 @@ public class TextDisplay : MonoBehaviour
         if (textStop == false) //テキストを表示させるif文
         {
             displayTextSpeed++;
-            if (displayTextSpeed % 3 == 0)//3回に一回プログラムを実行するif文
+            if (displayTextSpeed % 2 == 0)//2回に一回プログラムを実行するif文
             {
 
                 if (textCharNumber != texts[textNumber].Length)//もしtext[textNumber]の文字列の文字が最後の文字じゃなければ
@@ -94,9 +59,8 @@ public class TextDisplay : MonoBehaviour
                     }
                     else //もしtexts[]が最後のセリフになったら
                     {
-                        textend = true;
-                        //Bボタンが押されたら
-                        if (push)
+                        //テキストが表示されてからsecond待ったら
+                        if (timer > second)
                         {
                             displayText = ""; //表示させる文字列も消す
                             textCharNumber = 0; //文字の番号を最初にする
@@ -104,13 +68,11 @@ public class TextDisplay : MonoBehaviour
                             isTimer = false;
                             timer = 0;
                             texthaikei.enabled = false;
-                            ytext.enabled = true;
                         }
                     }
                 }
 
                 this.GetComponent<Text>().text = displayText;//画面上にdisplayTextを表示
-                //click = false;//クリックされた判定を解除
             }
 
 
