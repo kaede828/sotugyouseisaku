e661f18e5ae80d5ab020dde3ab06b90c42b705e1
Kawashita Kazuma <fled1127@gmail.com>
2021-11-19 16:11:47 +0900

    扉の変更

--- sotugyouseisaku/Assets/Iwatani/script/frieze.cs
+++ sotugyouseisaku/Assets/Iwatani/script/frieze.cs
@@ -13,6 +13,6 @@ public class frieze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, 0);
     }
 }
