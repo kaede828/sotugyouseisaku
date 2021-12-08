0c2826177b01bc41a30259409b8cf470cf62c1fb
hryuh1121 <h.ryuh1121@gmail.com>
2021-12-08 16:07:20 +0900

    エンディングのエレベーターを動くようにした

---
+++ sotugyouseisaku/Assets/Honda/ElevatorScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class ElevatorScript : MonoBehaviour
+{
+    //エレベーターのタイムラインをセット
+    [SerializeField] private TimelineAsset timeline;
+    private PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        director = this.GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            ElevatorUp();
+        }
+    }
+
+    //エレベーターを上にあげるタイムラインを再生
+    public void ElevatorUp()
+    {
+        director.Play(timeline);
+    }
+}
