517596435ad6ed64f36f2678e2e4d6ec17ba186d
佐藤幹 <->
2022-01-24 02:27:05 +0900

    敵がプレイヤーを見つけたときにSEが出るようにしたので被らないようにした。死亡時にSEが流れるのはまだ

--- sotugyouseisaku/Assets/Kan/Script/ZombieSound.cs
+++ sotugyouseisaku/Assets/Kan/Script/ZombieSound.cs
@@ -10,11 +10,57 @@ public class ZombieSound : MonoBehaviour
     private new AudioSource audio;
     // Start is called before the first frame update
 
+    private GameObject player;
+    private bool isplay;
+
+    [SerializeField]
+    private Vector3 point;
+
+    float count = 0.0f;
+    [SerializeField]
+    float stop = 4.0f;
+
     private void Start()
     {
         audio = GetComponent<AudioSource>();
         // BGMÇâπó 1.0Ç≈çƒê∂
-        Play(zombieSE, 1.0f);
+        //Play(zombieSE, 1.0f);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        isplay = false;
+        count = 0.0f;
+    }
+
+    private void Update()
+    {
+        var p = new Vector3(player.transform.position.x + point.x, player.transform.position.y + point.y, player.transform.position.z + point.z);
+        var d = new Vector3(gameObject.transform.position.x + point.x, gameObject.transform.position.y + point.y, gameObject.transform.position.z + point.z);
+        RaycastHit hit;
+        var dir = (p - d).normalized;
+        Debug.DrawRay(d, dir * 15.0f, Color.blue, 0.01f);
+        if (Physics.Raycast(d, dir, out hit, 30.0f))
+        {
+            if(hit.collider.gameObject.tag == "Player")
+            {
+                if(!isplay)
+                {
+                    //Debug.Log("on");
+                    isplay = true;
+                    Play(zombieSE, 1.0f);
+                }
+            }
+            else
+            {
+                //Debug.Log(hit.collider.gameObject.tag);
+                count += Time.deltaTime;
+                if(count > stop)
+                {
+                    Stop();
+                    isplay = false;
+                    count = 0.0f;
+                }
+            }
+        }
     }
 
     // 1. çƒê∂
